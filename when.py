48c24d81a5ad44f12f513e37803df7d38daab48d
Thibaud Colas <thibaudcolas@gmail.com>
2014-10-22 08:49:53 +0200

    Parameterized response

--- when.py
+++ when.py
@@ -6,14 +6,15 @@ from tornado.ioloop import IOLoop
 from tornado.web import Application, RequestHandler
 
 class MainHandler(RequestHandler):
-    def get(self):
-        self.output_message("Hello World!")
+    def get(self, message = None):
+        self.output_message(message)
 
     def output_message(self, message):
         self.render('index.html', message = message)
 
 application = Application([
     (r'/', MainHandler),
+    (r'/([a-z0-9]+)', MainHandler),
 ])
 
 if __name__ == '__main__':
