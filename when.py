c21497abac0bed30a5c593fa02205fe4f18667fe
Thibaud Colas <thibaudcolas@gmail.com>
2014-10-30 20:15:29 +0100

    Add file reader

--- when.py
+++ when.py
@@ -5,6 +5,18 @@ from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.web import Application, RequestHandler, StaticFileHandler
 
+try:
+    from hashlib import md5
+except ImportError:
+    from md5 import md5
+
+messages_file = os.path.join(os.path.dirname(__file__), 'messages.txt')
+messages = {}
+
+# Create a hash table of all commit messages
+for line in open(messages_file).readlines():
+    messages[md5(line).hexdigest()] = line
+
 class MainHandler(RequestHandler):
     def get(self, message = None):
         self.output_message(message)
